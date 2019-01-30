6deb6eebf94f8905402bef35241f6004adec7c47
PlatPlat48 <roberts.chance@gmail.com>
2019-01-30 12:29:02 -0700

    Map to Street View
    Created a link that moves from Map to Street View

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Street from './Street'
+import {Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 type State = {
   lat: number,
   lng: number,
   zoom: number,
+  img: string
 }
 
 export default class App extends Component {
@@ -12,22 +16,35 @@ export default class App extends Component {
     lat: 40.016869,
     lng: -105.279617,
     zoom: 13,
+    img: "sample_image.jpg"
   }
+
+  //<button onClick=""></button>
 
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <Map id='map' center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            Gotta go FHAST
-          </Popup>
-        </Marker>
-      </Map>
+      <div>
+        <Route exact path="/" render={()=>
+          <Map id='map' center={position} zoom={this.state.zoom}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                <Link to="/street">Gotta go FHAST</Link>
+                <p class="centered">
+                  <img width="64" height="64" src={this.state.img} alt=""/>
+                </p>
+              </Popup>
+            </Marker>
+          </Map>
+        }/>
+        <Route path="/street" render={()=>
+          <Street image={this.state.img}/>
+        } />
+      </div>
     )
   }
 }
