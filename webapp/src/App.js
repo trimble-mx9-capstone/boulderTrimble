0c3e9cc9b6fa12a01407a9ab9600fb46d1fdfc1e
Landon <laba2346@colorado.edu>
2019-01-31 12:26:22 -0700

    Moved navbar to its own react component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,36 +2,14 @@ import React, { Component } from 'react';
 import MapView from './MapView';
 import {Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import AppNav from './AppNav'
 
 export default class App extends Component {
-  state = {
-    lat: 40.016869,
-    lng: -105.279617,
-    zoom: 13,
-    img: "sample_image.jpg"
-  }
-
-  //<button onClick=""></button>
-
   render() {
     return (
       <div>
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Municipal Object Tracker</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Navbar>
-        <div id='main_container'>
+        <AppNav/>
+        <div id='main_container' class="shadow p-3 mb-5 bg-white rounded">
             <MapView/>
         </div>
       </div>
---
+++ webapp/src/AppNav.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import {Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+
+export default class AppNav extends Component {
+  render() {
+    return (
+    <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Municipal Object Tracker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Map</Nav.Link>
+                <Nav.Link href="#link">Add new images</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+        </Navbar.Collapse>
+    </Navbar>
+    )
+  }
+}
+
