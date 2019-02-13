ce2e9bc8c02bc06de93a613af4264a48065ea1a8
robert renecker <sylvan.rrenecker10@gmail.com>
2019-02-12 18:37:16 -0700

    deleted not needed code

--- processImages/collect_image_data.py
+++ processImages/collect_image_data.py
@@ -103,38 +103,6 @@ def main():
 
 
 
-    #This is the parent ID
-    parent_id = parent_pagnated_node['items'][0]['id'];
-    #Retrieve child node (MTX9 node)
-    pagnated_directory_nodes = hd.get_directory_by_id(host, parent_id, b_token);
-
-    #Now get the children for MTX9 Node --> Should have our URLS / image directories.
-    municipal_directory_parent_id = pagnated_directory_nodes['items'][0]['id'];
-    municipal_directory = hd.get_directory_by_id(host, municipal_directory_parent_id, b_token);
-
-    #Get URLS from node
-    url_for_images = {};
-    url_names = []
-
-    #Deal with adding city : raw_url to dictionary
-    for i,v in enumerate(municipal_directory['items']):
-        try:
-            print("Reading in url from %s node, url: %s " % (v['name'],v['download']['url']))
-            print("\n")
-            url_names.append(v['name']);
-            url_for_images.update({v['name']: v['download']['url']});
-        except KeyError as e:
-            print('KeyError on %s ' % e)
-
-    #Take raw URLs to nodes, find index file, return array of image URLS
-    image_urls, base_url,name = gi.parse_url(url_names[1], url_for_images[url_names[1]]);
-
-    #save images to local repo
-    pi.save_images(image_urls, base_url, name);
-
-
-    #unamed_run__O_Camera_360_0000 xx.jpg
-    #print(return_object)
 
 
 if  __name__ =='__main__':
